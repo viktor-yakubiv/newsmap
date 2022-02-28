2b5b9d8f83f47077ae6cbf94f9f7209759a25c36
Viktor Yakubiv <viktor@yakubiv.com>
2022-02-28 18:33:43 +0000

    Pass lint to post ✨

--- components/post.jsx
+++ components/post.jsx
@@ -2,8 +2,18 @@ import joinClassNames from 'classnames'
 
 const Post = ({ data, className, tag: Tag = 'div' }) => (
   <Tag className={joinClassNames(className)}>
-    <h2>{data.title}</h2>
+    <h2>
+      <a
+        href={data.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {data.title}
+      </a>
+    </h2>
+
     <p>{data.body}</p>
+
     <p>{data.locations.map(l => l.title).join('; ')}</p>
   </Tag>
 )
