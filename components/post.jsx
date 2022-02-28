2801ddde2381bde1909e589ff68ad686df7edc2c
Viktor Yakubiv <viktor@yakubiv.com>
2022-02-28 19:31:35 +0000

    Style Post 🎨

--- components/post.jsx
+++ components/post.jsx
@@ -1,21 +1,48 @@
 import joinClassNames from 'classnames'
+import styles from '../styles/post.module.css'
 
-const Post = ({ data, className, tag: Tag = 'div' }) => (
-  <Tag className={joinClassNames(className)}>
-    <h2>
-      <a
-        href={data.url}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {data.title}
-      </a>
-    </h2>
+const dateFormat = new Intl.DateTimeFormat('uk-UA', {
+  dateStyle: 'short',
+  timeStyle: 'short',
+})
+const formatDate = date => dateFormat.format(date)
 
-    <p>{data.body}</p>
+const Post = ({ data, className, tag: Tag = 'div' }) => {
+  const isSectioning = /section|article/ig.test(Tag)
+  const Header = isSectioning ? 'header' : 'div'
+  const Footer = isSectioning ? 'footer' : 'div'
 
-    <p>{data.locations.map(l => l.title).join('; ')}</p>
-  </Tag>
-)
+  return (
+    <Tag className={joinClassNames(styles.container, className)}>
+      <Header className={styles.header}>
+        <h2 className={styles.title}>
+          <a
+            href={data.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {data.title}
+          </a>
+        </h2>
+
+        <div className={styles.meta}>
+          <time dateTime={data.publicationDate.toISOString()}>
+            {formatDate(data.publicationDate)}
+          </time>
+        </div>
+      </Header>
+
+      <div className={styles.content}>
+        {data.body}
+      </div>
+
+      <Footer className={styles.footer}>
+        <p className={styles.location}>
+          {data.locations.map(l => l.title).join('; ')}
+        </p>
+      </Footer>
+    </Tag>
+  )
+}
 
 export default Post
