f366a84ef4b3c8a3a8d1be1f316d9e4897a798bf
Viktor Yakubiv <viktor@yakubiv.com>
2022-03-01 10:44:53 +0000

    Smart event handling for cards ✨

--- components/post.jsx
+++ components/post.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useRef } from 'react'
 import joinClassNames from 'classnames'
 import styles from '../styles/post.module.css'
 
@@ -8,12 +9,32 @@ const dateFormat = new Intl.DateTimeFormat('uk-UA', {
 const formatDate = date => dateFormat.format(date)
 
 const Post = ({ data, className, tag: Tag = 'div' }) => {
+  const containerRef = useRef(null)
+  const mouseDownTimeStamp = useRef(0)
+
   const isSectioning = /section|article/ig.test(Tag)
   const Header = isSectioning ? 'header' : 'div'
   const Footer = isSectioning ? 'footer' : 'div'
 
+  const recordMouseDown = useCallback((event) => {
+    mouseDownTimeStamp.current = event.timeStamp
+  }, [])
+
+  const clickLink = useCallback((event) => {
+    const clickDuration = event.timeStamp - mouseDownTimeStamp.current
+
+    if (clickDuration < 200 && containerRef?.current != null) {
+      containerRef.current.querySelector('h2 a')?.click()
+    }
+  }, [])
+
   return (
-    <Tag className={joinClassNames(styles.container, className)}>
+    <Tag
+      ref={containerRef}
+      className={joinClassNames(styles.container, className)}
+      onMouseDown={recordMouseDown}
+      onClick={clickLink}
+    >
       <Header className={styles.header}>
         <h2 className={styles.title}>
           <a
