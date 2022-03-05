d113d272286d191f2dc1b9137ba4511d064b6094
Viktor Yakubiv <viktor@yakubiv.com>
2022-03-05 22:04:36 +0000

    Fix date-time freshness calculation 🐛

--- data/schema/utils.js
+++ data/schema/utils.js
@@ -1,3 +1,4 @@
 const DAY = 24 * 60 * 60 * 1000 // ms
 
-export const calcFreshness = date => (Date.now() - date.getTime()) / DAY
+export const calcFreshness = (date, maxDuration = DAY) =>
+  1 - Math.min(1, (Date.now() - date.getTime()) / maxDuration)
