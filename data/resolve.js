27d9f134644c64c0b29c5dc78440b5e1012ecbfe
Viktor Yakubiv <viktor@yakubiv.com>
2022-03-09 17:06:38 +0100

    Better API parameters resolution ⚡️
    Prevent flickering of the application every 5 seconds as now it's
    every 5 minutes.
    
    The downside is that data for the latest 5 minutes is lost.

--- data/resolve.js
+++ data/resolve.js
@@ -1,32 +1,35 @@
+import { DURATION_MINUTE, DURATION_DAY } from '../utils/date/constants'
+
 const API_BASE = 'https://geonames.dev.socmonitor.com.ua/api/v1/news'
 
-const DURATION_1H = 60 * 60 * 1000 // ms
-const DURATION_6H = 6 * DURATION_1H
+// Indicates how often the new data is loaded
+// It should be large enough to prevent the application flickering
+// but small enough so the user gets fresh data;
+// the user won't see any post for the latest time period set below
+// — see how `round()` works and `until` parameter.
+const PRECISION = 5 * DURATION_MINUTE // ms
 
-const PRECISION = 5_000 // ms
 const round = t => Math.floor(t / PRECISION) * PRECISION
 
 const resolve = (params = {}) => {
   const now = round(Date.now())
   const {
     // Some precision required to prevent infinite request loop
-    since = new Date(now - DURATION_6H).toISOString(),
+    since = new Date(now - DURATION_DAY).toISOString(),
     until = new Date(now).toISOString(),
-    query = '*',
+    query,
     token,
   } = params
 
   const url = new URL(API_BASE)
 
   // Configuration
-  url.searchParams.set('from_date', since)
-  url.searchParams.set('end_date', until)
-  url.searchParams.set('text', query)
+  if (since) url.searchParams.set('from_date', since)
+  if (until) url.searchParams.set('end_date', until)
+  if (query) url.searchParams.set('text', query)
 
   // Authorization side-effect
-  if (token) {
-    url.searchParams.set('access_token', token)
-  }
+  if (token) url.searchParams.set('access_token', token)
 
   return url.href
 }
