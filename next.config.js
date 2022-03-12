705e48c896711ad2d0610dbae31d03f96bb9cde2
Viktor Yakubiv <viktor@yakubiv.com>
2022-03-12 13:55:15 +0100

    Icon sprites ✨

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
+const path = require('path')
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack(config) {
+    const nextImagesRule = config.module.rules
+      .find(({ loader }) => loader === 'next-image-loader')
+    nextImagesRule.exclude = /\.icon\.svg$/i
+
+    config.module.rules.push({
+      test: /\.icon\.svg$/i,
+      loader: 'svg-sprite-loader',
+      options: {
+        symbolId: (filePath) => path.basename(filePath, '.icon.svg'),
+        extract: true,
+        spriteFilename: 'icons-[hash:16].svg'
+      },
+    })
+
+    config.plugins.push(new SpriteLoaderPlugin())
+
+    return config
+  }
 }
 
 module.exports = nextConfig
