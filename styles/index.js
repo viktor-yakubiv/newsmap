d29e29925e79d5faf669dc62b971dfb447980152
Viktor Yakubiv <viktor@yakubiv.com>
2022-03-16 12:41:15 +0100

    Typography 🎨

---
+++ styles/index.js
@@ -0,0 +1,14 @@
+import typography from './typography.module.css'
+import layout from './layout.module.css'
+
+const all = {
+  ...typography,
+  typography,
+  layout,
+}
+
+export default all
+export {
+  typography,
+  layout,
+}
